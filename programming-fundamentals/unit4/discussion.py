81fd5ae9c0f33d6b437060656487b1ff566a2e1e
kentotakeuchi <ideal.kt.reality@gmail.com>
2022-07-09 03:01:17 -0700

    wip: unit4 > discussion

---
+++ programming-fundamentals/unit4/discussion.py
@@ -0,0 +1,7 @@
+'''
+Section 6.9 of your textbook ("Debugging") lists three possibilities to consider if a function is not working.
+
+1. Describe each possibility in your own words.
+2. Define "precondition" and "postcondition" as part of your description.
+3. Create your own example of each possibility in Python code. List the code for each example, along with sample output from trying to run it.
+'''
