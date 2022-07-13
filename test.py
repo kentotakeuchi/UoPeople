c2d315e4881880c3d25465af570b231e8217af4c
kentotakeuchi <ideal.kt.reality@gmail.com>
2022-07-12 23:51:01 -0700

    done: unit4 > dis > peer

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-print('hi')
+# def test_function(l, w, h):
+#     print("the volume of the box is ", l*w*h)
+#     return l*w*h
+
+
+# test_function(2, 3, 5)
+
+
+def test_function(l, w, h):
+    print("the result of the operation is ", l*w/h)
+    return l*w*h
+
+
+test_function(2, 3, 0)
