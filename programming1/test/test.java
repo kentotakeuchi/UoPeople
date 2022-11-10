e682c68000daeb12ee0815d0304c4274baf5c3f6
kentotakeuchi <ideal.kt.reality@gmail.com>
2022-11-10 15:48:31 -0800

    done: set up R for intro of stats

--- programming1/test/test.java
+++ programming1/test/test.java
@@ -5,8 +5,8 @@ import java.util.*;
 class ArrayGames {
     public static void main(String[] args) {
         int[] a = { 1, 2, 3, 4, 5 };
-        for (int i = 1; i < a.length; i++)
-            a[i] = a[i - 1];
+        for (int i = 0; i < a.length; i++)
+            a[i] += a[i];
         System.out.println(Arrays.toString(a));
     }
 }
