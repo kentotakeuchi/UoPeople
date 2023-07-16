18804d4d23266ffa444009a55de3396d52b2a095
kentotakeuchi <ideal.kt.reality@gmail.com>
2023-07-15 21:09:24 -0700

    done: sys & data > unit4 > peer

--- AlgorithmComparison.java
+++ AlgorithmComparison.java
@@ -1,15 +1,87 @@
-public class AlgorithmComparison {
-    public static void main(String[] args) {
-        int n = 1;
 
-        // Double.compare(double d1, double d2)
-        // d1 = d2 -> return 0
-        // d1 < d2 -> return less than 0
-        // d1 > d2 -> return greater than 0
-        while (Double.compare(Math.pow(2, n), 1000 * Math.pow(n, 3)) < 0) {
-            n++;
+// import Prog1Tools.IOTools;
+import java.util.*;
+
+public class BtreeAlgorithm {
+    public static class btreeNode {
+        btreeNode left;
+        btreeNode right;
+        int value;
+
+        public btreeNode(int value) {
+            this.value = value;
         }
+    }
 
-        System.out.println("The first integer of n that '1000n^3 < 2^n' is: " + n);
+    public static void main(String[] args) {
+        btreeNode link, root;
+        int treeValue;
+        int searchValue;
+        int iterations;
+        iterations = 0;
+        root = null;
+        link = null;
+        Scanner in = new Scanner(System.in);
+        // build binary tree, add node
+        do {
+            System.out.println("Enter value to populate tree with: ");
+            treeValue = in.nextInt();
+            if (treeValue > 0) {
+                if (root != null) {
+                    addNode(root, treeValue);
+                } else {
+                    root = new btreeNode(treeValue);
+                }
+            }
+        } while (treeValue > 0);
+        System.out.println("Enter search value : ");
+        searchValue = in.nextInt();
+        iterations = findNode(root, searchValue, iterations);
+        if (iterations == 0) {
+            System.out.println("Search value not found");
+        } else {
+            System.out.println("Value found in " + iterations + " iterations");
+        }
+    }
+
+    // Value search
+    public static int findNode(btreeNode node, int value, int iter) {
+        if (node != null) {
+            if (value == node.value) {
+                iter += 1;
+            } else if (value < node.value) {
+                iter += 1;
+                iter = findNode(node.left, value, iter);
+
+            } else {
+                iter += 1;
+                iter = findNode(node.right, value, iter);
+            }
+        }
+        return (iter);
+    }
+
+    // Search tree node add
+    public static void addNode(btreeNode node, int value) {
+        btreeNode pnode;
+        pnode = null;
+        if (value < node.value) {
+            if (node.left != null) {
+                addNode(node.left, value);
+            } else {
+                System.out.println(" Inserted " + value + " to left of " +
+                        node.value);
+                node.left = new btreeNode(value);
+            }
+        } else if (value > node.value) {
+            if (node.right != null) {
+                addNode(node.right, value);
+            } else {
+                System.out.println(" Inserted " + value + " to right of " +
+                        node.value);
+                node.right = new btreeNode(value);
+
+            }
+        }
     }
 }
---
+++ data-structure/unit4/solution/unit_4_assignment_btree.java
@@ -0,0 +1,96 @@
+import Prog1Tools.IOTools;
+import java.util.*;
+
+class btreeNode {
+    btreeNode left;
+    btreeNode right;
+    int value;
+
+    public btreeNode(int value) {
+        this.value = value;
+    }
+}
+
+public class BtreeAlgorithm {
+
+    public static void main(String[] args) {
+        btreeNode link, root;
+        int treeValue;
+        int searchValue;
+        int iterations;
+
+        iterations = 0;
+        root = null;
+        link = null;
+        Scanner in = new Scanner(System.in);
+
+        //
+        // build binary tree by adding nodes to tree structure, then search for item in
+        // tree
+        //
+        do {
+            System.out.println("Enter value to populate tree with: ");
+            treeValue = in.nextInt();
+
+            if (treeValue > 0) {
+                if (root != null) {
+                    addNode(root, treeValue);
+                } else {
+                    root = new btreeNode(treeValue);
+                }
+            }
+        } while (treeValue > 0);
+
+        System.out.println("Enter search value : ");
+        searchValue = in.nextInt();
+        iterations = findNode(root, searchValue, iterations);
+        if (iterations == 0) {
+            System.out.println("Search value not found");
+        } else {
+            System.out.println("Value found in " + iterations + " iterations");
+        }
+    }
+
+    //
+    // Find value in binary tree
+    //
+    public static int findNode(btreeNode node, int value, int iter) {
+        if (node != null) {
+            if (value == node.value) {
+                iter += 1;
+            } else if (value < node.value) {
+                iter += 1;
+                iter = findNode(node.left, value, iter);
+            } else {
+                iter += 1;
+                iter = findNode(node.right, value, iter);
+            }
+        }
+        return (iter);
+    }
+
+    //
+    // Add node to binary searhc tree
+    //
+    public static void addNode(btreeNode node, int value) {
+        btreeNode pnode;
+        pnode = null;
+
+        if (value < node.value) {
+            if (node.left != null) {
+                addNode(node.left, value);
+            } else {
+                System.out.println("  Inserted " + value + " to left of " + node.value);
+                node.left = new btreeNode(value);
+            }
+        } else if (value > node.value) {
+            if (node.right != null) {
+                addNode(node.right, value);
+            } else {
+                System.out.println("  Inserted " + value + " to right of " + node.value);
+                node.right = new btreeNode(value);
+            }
+        }
+    }
+
+}
